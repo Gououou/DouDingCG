558266a295937176e274eea0849df0ddec16c479
Guo <1429503702@qq.com>
2024-04-25 14:41:46 +0800

    [20240425]新建项目，测试推送

---
+++ src/main/java/com/douding/doudingcg/DouDingCgApplication.java
@@ -0,0 +1,13 @@
+package com.douding.doudingcg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DouDingCgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DouDingCgApplication.class, args);
+    }
+
+}
